4479e42c8eaf64f9393d31540e9f006297daa46a
maciej <maciej.krzywda@cyfroweszkoly.pl>
2026-03-10 14:29:22 +0100

    Dodanie pojazdów do json, zmiana VehicleFuel na double (0.9 = 90%)

--- FleetManager/Models/Vehicle.cs
+++ FleetManager/Models/Vehicle.cs
@@ -1,6 +1,12 @@
 namespace FleetManager.Models;
+using ReactiveUI;
+using ReactiveUI.Fody.Helpers;
 
-public class Vehicle
+public class Vehicle : ReactiveObject
 {
-    
+    [Reactive] public int VehicleId { get; set; }
+    [Reactive] public string VehicleName { get; set; } = string.Empty;
+    [Reactive] public string VehicleTag { get; set; } = string.Empty;
+    [Reactive] public double VehicleFuel { get; set; }
+    [Reactive] public string VehicleStatus { get; set; } = string.Empty;
 }
