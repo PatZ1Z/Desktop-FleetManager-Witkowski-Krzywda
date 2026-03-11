4c8ed2cd35c3106a1648ae45e4df66bd05f334c0
PatZ1Z <patryk.witkowski@cyfroweszkoly.pl>
2026-03-11 11:37:45 +0100

    Stworzenie klasy Vehicle, Dodanie listy wyświetlającej pojazdy.

--- FleetManager/ViewModels/MainWindowViewModel.cs
+++ FleetManager/ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,41 @@
-﻿namespace FleetManager.ViewModels;
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Text.Json;
+using FleetManager.Models;
+
+namespace FleetManager.ViewModels;
 
 public class MainWindowViewModel : ViewModelBase
 {
-    public string Greeting { get; } = "Welcome to Avalonia!";
+    public ObservableCollection<Vehicle> Vechicles { get; set; } = [];
+    
+    private const string FilePath = "Data/vechicles.json";
+
+    public MainWindowViewModel()
+    {
+        
+    }
+    
+    private void LoadCharacters()
+    {
+        if(!File.Exists(FilePath)) return;
+        try
+        {
+            var jsonData = File.ReadAllText(FilePath);
+            var list = JsonSerializer.Deserialize<List<Vehicle>>(jsonData);
+            Vechicles.Clear();
+            if (list == null) return;
+            foreach (var vechicle in list)
+            {
+                Vechicles.Add(vechicle);
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+        }
+        
+    }
 }
